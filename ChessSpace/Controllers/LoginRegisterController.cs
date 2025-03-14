64917f99cee7cc5bad31c417fe94fd2009b443ab
Jonsua <joshua.mobieltje@gmail.com>
2025-03-14 16:26:21 +0100

    loginpage plus styling

---
+++ ChessSpace/Controllers/LoginRegisterController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChessSpace.Controllers {
+    public class LoginRegisterController : Controller {
+        public IActionResult Login() {
+            return View(); 
+        }
+
+        public IActionResult Register() {
+            return View(); 
+        }
+    }
+
+}
